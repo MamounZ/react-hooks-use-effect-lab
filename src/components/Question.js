608105c0ab4c726ef8aec83bc6721fc281bad2a2
Mamoun Zaid <mam.j.zeid@gmail.com>
2026-03-04 23:04:45 +0300

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  useEffect(()=>{
+    const intervalId = setInterval(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1);
+    }, 1000);
+
+    return function() {
+      clearInterval(intervalId)
+    }
+  }
+  ,[])
+
+  useEffect(() =>{
+  if (timeRemaining === 0){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }}, [timeRemaining, onAnswered])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
